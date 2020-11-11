a070682b3cce2c009e150b8949941994346578cc
hamzahellowworld <hamzaaitsidisaid.11@gmail.com>
2020-11-11 21:31:32 +0000

    javascript bugg

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,12 +1,15 @@
+
 document.addEventListener("DOMContentLoaded", function(event) { 
     setBlogTile('Deploy Laravel Project On Heroku',0)
   });
+
 
 function setBlogTile(title,content)
 {
    getBlogContent(content);
    $('#card-header-title').html('<strong id="title">'+title+'</strong>');
 }
+
 
 function getBlogContent(content)
 {
